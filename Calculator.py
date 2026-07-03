e97a2d630c5135c09c15ebb7d15f97b5be2144bc
Abdur Rehman Subhani <153990801+abdur-rehman-subhani@users.noreply.github.com>
2026-07-03 22:07:13 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+a=35
+b=7
+sum=a+b
+subtract=a-b
+multiply=a*b
+divide=a/b
+print("The Sum of ",a," and",b, "is: ", sum)
+print("The Subtraction of ",a," and",b, "is: ", subtract)
+print("The Multiplication of ",a," and",b, "is: ", multiply)
+print("The Division of ",a," and",b, "is: ", divide)
---
+++ Data Types.py
@@ -0,0 +1,17 @@
+a=12
+b=2.5
+c="Harry"
+d=True
+e=("Harry", 62, 33.4) # Tuples are immuteable and can't be modefied after creation 
+f={}
+g=[8,"Harry", 23.5,["David, 34.5"]] #List is basically collection of data that can be of diff Types 
+h=complex(23,4) #Complex numbers(one real number and other is imaginary )
+print("The Data Type of a is: ", type(a))
+print("The Data Type of b is: ", type(b))
+print("The Data Type of c is: ", type(c))
+print("The Data Type of d is: ", type(d))
+print("The Data Type of e is: ", type(e))
+print("The Data Type of f is: ", type(f))
+print("The Data Type of g is: ", type(g))
+print("The Data Type of h is: ", type(h))
+#This is a comment 

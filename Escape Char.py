e71585d385086193f30f1395b00dffa1501950d4
Abdur Rehman Subhani <153990801+abdur-rehman-subhani@users.noreply.github.com>
2026-07-03 22:10:02 +0500

    Add files via upload

---
+++ Escape Char.py
@@ -0,0 +1,13 @@
+#This is a Single Line Comment 
+"""
+This is 
+a 
+Multi- Line 
+Comment 
+"""
+
+print("Hello World")
+print("Hey, I am Abdur Rehman ",23,4) 
+print(13+3)
+print("I am Learning \n Python\t and C++")
+print("I am \"Good Boy\" ",2, 78, sep="-", end="007")

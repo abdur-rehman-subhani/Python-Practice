2f7968d0aa2be1e755e84e8a0ac446adbe8dbb5c
Abdur Rehman Subhani <153990801+abdur-rehman-subhani@users.noreply.github.com>
2026-07-03 22:09:08 +0500

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,7 @@
+#In python we have different operators that we can use to perform calculations
+print(12+3)
+print(12-3)
+print(12*3)
+print(12/3)
+print(12//3) #Floor Division Operator. It gives us the answer in integer form not in float
+print(2**4) # Exponentiol Operator. Here it is meant as 2 power 4 

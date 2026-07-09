aeca5920c9737b63dc63556afbbcc89cf7830a30
Abdur Rehman Subhani <153990801+abdur-rehman-subhani@users.noreply.github.com>
2026-07-10 00:58:52 +0500

    Add files via upload

---
+++ For loop.py
@@ -0,0 +1,23 @@
+name="David"
+for i in name:
+    if (i=="d"):
+        print("This d is special in david ")
+    print(i)
+    
+colors=['Red','Blue','Green','Black']
+for color in colors:
+    print(color)
+    for chars in color:
+        print(chars)
+
+# Prints number upto 20
+for i in range(0,20):
+  print(i+1)
+
+# Tabel of 2
+j=2
+for i in range(0,10):
+  print(j,"*",i+1,"=",j*(i+1))
+
+for i in range(1,10, 1):
+   print(i)

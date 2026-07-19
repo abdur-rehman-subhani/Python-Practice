896b154d258881d95b7e4d5075b48c35ce78984a
Abdur Rehman Subhani <153990801+abdur-rehman-subhani@users.noreply.github.com>
2026-07-20 01:27:56 +0500

    Add files via upload

---
+++ Sets in python.py
@@ -0,0 +1,16 @@
+s={21,2,3,3,2,8}
+print(s)
+
+# Different ype of data can be used in sets like lists
+info={"carla", True, 2,71.2}
+print(info)
+for i in info:
+    print(i)
+
+#-----> Create an Empty Set
+empSet={}
+print(type(empSet)) # Here Type=dict
+
+#The Correct Method to creae an empty set
+mySet=set()
+print(type(mySet)) # Here Type= Set

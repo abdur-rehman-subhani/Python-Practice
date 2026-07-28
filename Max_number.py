b8c844d4312fd76bc5e7f78eed37726aefb7d65b
Abdur Rehman Subhani <153990801+abdur-rehman-subhani@users.noreply.github.com>
2026-07-29 02:18:57 +0500

    Add files via upload

---
+++ Max_number.py
@@ -0,0 +1,25 @@
+def max_in_list(a):
+    print("The largest element in the list is: ",max(a))
+
+def min_in_list(b):
+    print("The smallest element in the list is: ",min(b))
+
+def sum_of_List(c):
+    print("The sum of all elements in list is:",sum(c))
+
+list=[122,13,3,35,36,78,657,54,8]
+max_in_list(list)
+min_in_list(list)
+sum_of_List(list)
+
+list2=[24,43,214,42,35,57,456,1,]
+max_in_list(list2)
+min_in_list(list2)
+sum_of_List(list2)
+print(list.append[list2])
+list3=["Ali", "hasan","haseeb"]
+max_in_list(list3)
+min_in_list(list3)
+# sum_of_List(list3) --> This will throw an error
+
+
